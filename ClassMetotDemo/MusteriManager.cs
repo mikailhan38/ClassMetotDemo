24043375ccf7db32b5d44b4de7f7fcdc08c16307
mlale <mlale@KAY-MLALE>
2023-03-08 15:41:50 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name + " " + musteri.LastName + " Kayıt işlemleri yapıldı");
+        }
+        public void Sil(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri Kaydı silindi.Silinen Müşteri :" + musteri.Name + " " + musteri.LastName);
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Ad :"+musteri.Name);
+            Console.WriteLine("Soyad :" + musteri.LastName);
+            Console.WriteLine("Hesap Bakiyesi :" + musteri.Price);
+            Console.WriteLine("________________________");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using ClassMetotDemo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Musteri musteri1 = new Musteri();
+        musteri1.Name = "Mikail";
+        musteri1.LastName = "Lale";
+        musteri1.Price = 100;
+        Musteri musteri2 = new Musteri();
+        musteri2.Name = "Tuncay";
+        musteri2.LastName = "Lale";
+        musteri2.Price = 1000;
+        Musteri musteri3 = new Musteri();
+        musteri3.Name = "Muhammed";
+        musteri3.LastName = "Lale";
+        musteri3.Price = 1205;
+        Musteri musteri4 = new Musteri();
+        musteri4.Name = "Fatıma";
+        musteri4.LastName = "Lale";
+        musteri4.Price = 4500;
+
+        
+
+        Musteri[] musteriler = new Musteri[]
+        {
+            musteri1, musteri2, musteri3, musteri4
+        };
+        Console.WriteLine("Müşteriler Listelendi \n ------------------------");
+        MusteriManager musteriManager = new MusteriManager();
+        foreach (var item in musteriler)
+        {
+            musteriManager.Listele(item);
+        }
+
+        musteriManager.Ekle(musteri1);
+        Console.WriteLine("--------------");
+        musteriManager.Sil(musteri3);
+    }
+}
